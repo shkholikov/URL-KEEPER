0076256efeb90a3f3c67bf370bc92be24401162f
Shakhzod Kholikov <51167612+shkholikov@users.noreply.github.com>
2024-03-04 11:52:02 +0100

    Update email.ts

--- email.ts
+++ email.ts
@@ -1,4 +1,5 @@
 function validateEmail(email: string): boolean {
   const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const regex = /^[^@\s]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   return regex.test(email);
 }
